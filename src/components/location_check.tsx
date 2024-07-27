5f9a48e073c66e18b3a3afa14fe5658c17e5c598
Prayus04 <prayus.sharma@edu.sait.ca>
2024-07-26 20:46:13 -0600

    Updated Location_check to add fun facts

--- src/components/location_check.tsx
+++ src/components/location_check.tsx
@@ -1,15 +1,34 @@
 import {Text} from 'react-native';
 
-async function getLocationData(location: string) {
-  let urlString =
-    'https://geocoding-api.open-meteo.com/v1/search?name=' +
-    location +
-    '&count=10&language=en&format=json';
+export async function getLocationData(location : string) {
+  const urlString = `https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`;
+  
+  const response = await fetch(urlString);
+  const results = await response.json();
+  
+  const coordinates = [
+    results.results[0].latitude,
+    results.results[0].longitude,
+    results.results[0].country,
+  ];
 
-  let response = await fetch(urlString);
-  let results = await response.json();
-
-  let latitude = results[0].lat;
+  // console.log(coordinates);
+  return coordinates;
 }
 
-export default getLocationData;
+ //load current device location coordinates into the app
+export async function getWeatherData(latitude : number, longitude : number) {
+  const urlString = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,is_day,weather_code&daily=weather_code`;
+  
+  const response = await fetch(urlString);
+  const results = await response.json();
+  
+  const weatherData = [
+    results.current.temperature_2m,
+    results.current.apparent_temperature,
+    results.current.is_day,
+    results.current.weather_code,
+  ];
+
+  return weatherData;
+}
